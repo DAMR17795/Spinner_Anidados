0b6344e9598fc8018000e06a77f19b425f5b4f44
Daniel <dmarrom577@g.educaand.es>
2022-12-11 14:36:04 +0100

    Version casa

--- app/src/main/java/www/iesmurgi/spinner_anidados/MainActivity.kt
+++ app/src/main/java/www/iesmurgi/spinner_anidados/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter
 import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
+    //Creacion variables
     private var spinnerFam: Spinner?= null
     private var spinnerCic: Spinner?= null
 
@@ -15,25 +16,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Asignacion Variables
         spinnerFam = findViewById(R.id.spinnerFamilias)
         spinnerCic = findViewById(R.id.spinnerCiclos)
 
+        //Adaptador para el primer Spinner-Familias
         var adaptador: ArrayAdapter<CharSequence> =
             ArrayAdapter.createFromResource(
                 this, R.array.array_familias, android.R.layout.simple_spinner_item)
+
+        //layout para mostrar las familias
         adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        //Asignacion de adaptador a Spinner
         spinnerFam?.adapter=adaptador
+
+        //Metodo Listener para asignar el segundo adaptador y spinner
         spinnerFam?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
 
+                //Array con los ciclos
                val arrayCiclos = arrayOf(R.array.array_mecanica, R.array.array_informatica, R.array.array_administracion
                , R.array.array_electricidad)
 
+                //Segundo adaptador para spinnerCic
                 var adaptador2: ArrayAdapter<CharSequence> =
                     ArrayAdapter.createFromResource(this@MainActivity, arrayCiclos[p2],
                     android.R.layout.simple_spinner_item)
 
+                //layout para mostrar los ciclos
                 adaptador2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+                //Asignacion de adaptador a spinner
                 spinnerCic?.adapter=adaptador2
 
 
